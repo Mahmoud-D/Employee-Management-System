9d04609093ad7c378bc35e8330da93a6db134b5f
Mahmoud Ahmed <64172597+Mahmoud-D@users.noreply.github.com>
2024-05-06 17:13:11 +0300

    ADD Header file

---
+++ EmployeeMangament.h
@@ -0,0 +1,47 @@
+// Header File
+#include <iostream>
+using namespace std;
+
+class Employee {
+// data member
+private:
+    // Node Class
+    class Node {
+    public:
+        int key;
+        string Name;
+        string Dept;
+        int Salary;
+        Node* next;
+    };
+    Node *head, *prev, *cursor;
+
+// function member
+public:
+//Constractor
+    Employee();
+    bool listIsEmpty();
+    bool cursorEmpty();
+    void toFirst();
+    void Advance();
+    bool atFirst();
+    void toEnd();
+    bool atEnd();
+    int listSize();
+    bool search(const int &id);
+// Function to insert the record
+    void Insert_Record(int id, string Name,	string Dept, int Salary);
+    int Delete_Record(int id);
+// Function to display the Employee's
+    void Search_Record(int id);
+// Record
+    void Show_Record();
+};
+//
+// Created by YN on 5/6/2024.
+//
+
+#ifndef EMPLOYEEMANGAMENT_EMPLOYEEMANGAMENT_H
+#define EMPLOYEEMANGAMENT_EMPLOYEEMANGAMENT_H
+
+#endif //EMPLOYEEMANGAMENT_EMPLOYEEMANGAMENT_H
